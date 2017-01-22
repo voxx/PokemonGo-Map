e0fc2be4581ca41eb05e592bfffb5a2514f1fcca
voxx <cd@goldencomm.com>
2017-01-22 08:08:05 -0800

    Change log level for -tut from debug to info and increase sleep delay again.

--- pogom/search.py
+++ pogom/search.py
@@ -797,11 +797,11 @@ def search_worker_thread(args, account_queue, account_failures, search_items_que
                         tutorial_state = get_tutorial_state(api)
 
                         if not all(x in tutorial_state for x in (0, 1, 3, 4, 7)):
-                            log.debug(
+                            log.info(
                                 'Completing tutorial steps for %s.', account['username'])
                             complete_tutorial(api, account, tutorial_state)
                         else:
-                            log.debug(
+                            log.info(
                                 'Account %s has completed tutorial.', account['username'])
 
                 # Putting this message after the check_login so the messages
--- pogom/utils.py
+++ pogom/utils.py
@@ -722,5 +722,5 @@ def complete_tutorial(api, account, tutorial_state):
         time.sleep(random.uniform(0.8, 1.8))
 
     # Sleeping before we start scanning to avoid Niantic throttling.
-    time.sleep(random.uniform(5, 10))
+    time.sleep(random.uniform(7, 12))
     return True
