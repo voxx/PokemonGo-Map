5dd484b559ca97d7ba3e1985c1fd76f016a88924
voxx <cd@goldencomm.com>
2017-04-09 16:33:30 -0700

    Ditto finder logic. (DO NOT PULL - Not finished, just testing)

--- pogom/models.py
+++ pogom/models.py
@@ -1976,7 +1976,7 @@ def parse_map(args, map_dict, step_location, db_update_queue, wh_update_queue,
                 if int(pid) in ditto_dex:
                     log.info('%s may be a ditto. Triggering catch logic!', pid)
 
-                    ditto_result = catch(api, str(p['encounter_id']), str(p['spawn_point_id']), pid)
+                    ditto_result = catch(api, p['encounter_id'], p['spawn_point_id'], pid)
                     ditto_data = json.loads(ditto_result)
                     if 'catch_result' in ditto_data['data'][0] and ditto_data['data'][0]['catch_result'] == 'success':
                         if int(ditto_data['data'][0]['cpid']) == 132:
